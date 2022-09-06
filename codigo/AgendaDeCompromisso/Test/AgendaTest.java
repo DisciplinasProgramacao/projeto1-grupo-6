449e3431c3f2fb43f3f1c48525064189668881a7
Kaio Henrique Silveira <83793997+kaiohs333@users.noreply.github.com>
2022-09-05 22:07:24 -0300

    Update AgendaTeste - Teste para método de listar compromissos
    Foi criado o teste para a listagem ordenada de compromissos, mas ainda falta ajustes para que o teste fique 100%.

--- codigo/AgendaDeCompromisso/Test/AgendaTest.java
+++ codigo/AgendaDeCompromisso/Test/AgendaTest.java
@@ -7,21 +7,41 @@ public class AgendaTest {
 
     Data data1;
     Data data2;
+    Data data3;
+    Data data4;
+    Data data5;
+    
+    Agenda agenda = new Agenda();
 
     @BeforeEach
     public void init() throws InvalidAttributeValueException {
         data1 = new Data(20, 7,2022);
         data2 = new Data(19, 12, 2022);
+        data3 = new Data(15, 10, 2022);
+        data4 = new Data(19, 10, 2022);
+        data5 = new Data(15, 11, 2022);
+        
+        agenda.adcionarCompromisso("Dentista", data1, 2, 2);
+        agenda.adcionarCompromisso("Ir ao mercado", data2, 0, 1);
+        agenda.adcionarCompromisso("Reunião", data3, 0, 1);
+        agenda.adcionarCompromisso("Casamento", data4, 0, 1);
+        agenda.adcionarCompromisso("Viagem", data5, 0, 1);
 
     }
 
     @Test
     public void criarCompromisso() throws InvalidAttributeValueException {
 
-        Agenda agenda = new Agenda();
-        agenda.adcionarCompromisso("Dentista", data1, 2, 2);
-        agenda.adcionarCompromisso("Ir ao mercado", data2, 0, 1);
+        
         assertEquals("1-Dentista, 20/07/2022\n2-Dentista, 22/07/2022\n3-Ir ao mercado, 19/12/2022\n", agenda.verTodosCompromissos());
+
+    }
+    
+        @Test
+        public void listarCompromissos() throws InvalidAttributeValueException {
+
+        assertEquals("Compromisso - Reunião: 15/10/2022\nCompromisso - Casamento: 19/10/2022\nCompromisso - Viagem: 15/11/2022", agenda.listarCompromissos(new Data(15, 10, 2022), new Data(15, 11, 2022)) );
+
 
     }
 
